4b1c24676045903f8d664b6286224d75065664be
cameronkunstadt <cameron.kunstadt@gmail.com>
2025-02-27 17:10:21 -0800

    added argparse and made gitignore file

---
+++ motif-mark-oop.py
@@ -0,0 +1,10 @@
+# Cameron Kunstadt
+# UO import debugpy, platform
+# 2/27/2025
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--fasta",)  
+parser.add_argument('-m', "--motif")
+args = parser.parse_args()
